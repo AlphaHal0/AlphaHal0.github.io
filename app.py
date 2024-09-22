f1f3c2cbc838b2cb378d2b87a76866c98312b3f4
FrogFlavouredWater <hugowoolrichburt@gmail.com>
2024-09-22 22:30:00 +0100

    mobile

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+import logging
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    logging.info("Rendering Template")
     return render_template('mobile.html')
 
+app.run(debug=True)
